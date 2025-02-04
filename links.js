1e056b724052ab0e921e7493fe540819237809c4
atlmendoza <94661595+atlmendoza@users.noreply.github.com>
2025-02-05 07:15:48 +0800

    Update links.js

--- links.js
+++ links.js
@@ -10,38 +10,7 @@ links[5]={section:"9Rb",title:"Genshin Impact: Four Archons",url:"https://index.
 links[6]={section:"9Rb",title:"Adele - A Celebrity Among Celebrities",url:"https://rbmuallilmarte.b2026sdmarte.repl.co/",icon:"adele.png"}
 links[7]={section:"9Rb",title:"Earth's Breath",url:"https://rb20perezrb22ramoscs3project.christine-zoezo.repl.co/",icon:"earth.png"}
 links[8]={section:"9Rb",title:"Game Hub",url:"https://rbrespiciotainohtml.lorenzo-manuelm.repl.co/",icon:"gamehub.png"}
-links[9]={section:"9Li",title:"Famous Filipino Food",url:"https://liavena2ndqtrproj.orbitrust.repl.co/",icon:"filfood.png"}
-links[10]={section:"9Li",title:"Your Mental Health Helpline",url:"https://cs3projectbasobas-tan.alessandra11067.repl.co/htdocs/webpage1.html",icon:"mentalhealth.png"}
-links[11]={section:"9Li",title:"SOGIExplained",url:"https://sogiexplained.ethan-geoffreyg.repl.co/index.html",icon:"sogie.png"}
-links[12]={section:"9Li",title:"first.fm - Find More Music",url:"https://matti83c.github.io/Li-Campos-Palomar/index.html",icon:"fm.png"}
-links[13]={section:"9Li",title:"Chronicles of Mobile Gaming",url:"https://yukikori.github.io/LiDE-CASTRO_DAGSA/",icon:"mobile.png"}
-links[14]={section:"9Li",title:"Climatron",url:"https://lithiumgarciaoctaviano1stqtrproj.christian-brand.repl.co/",icon:"climatron.png"}
-links[15]={section:"9Li",title:"KDramaChingu",url:"https://lifernandezjoseph1stqtrproj.rika-gianagiana.repl.co/",icon:"kdrama.png"}
-links[16]={section:"9Mg",title:"Ad Astra Aspera",url:"https://mgcanicosiydevrepl.kurt-travistrav.repl.co/Webpages/Explorers.html",icon:"adastra.png"}
-links[17]={section:"9Mg",title:"Simply Cube",url:"https://alec-reyes.github.io/MgReyesCastillio/",icon:"cube.png"}
-links[18]={section:"9Mg",title:"Isshows.com",url:"https://isshowscom.gabr1elle.repl.co/",icon:"isshow.png"}
-links[19]={section:"9Mg",title:"I-Pick-It",url:"https://akgestrada.github.io/MgEstrada1stQtrProj/index.html",icon:"ipickit.png"}
-links[20]={section:"9Mg",title:"A Comprehensive Guide to F1",url:"https://a-comprehensive-guide-to-f1.takuhaya.repl.co/",icon:"f1.png"}
-links[21]={section:"9Mg",title:"Marcos In The Pages Of History",url:"https://cs3project.kilyan-ryerye.repl.co/",icon:"marcos.png"}
-links[22]={section:"9Mg",title:"Astrinity Encyclopedia of the Stars",url:"https://cs3-projectmgparajas-trompeta.sjsparajas.repl.co/",icon:"astrinity.png"}
-links[23]={section:"9Mg",title:"Human Inside",url:"https://ourwebsite.matthew-jeightj.repl.co/index.html",icon:"human.png"}
-links[24]={section:"9Mg",title:"Our Pisay Life Diaries ",url:"https://mggatbontonandpalacol1stqtrproj.raissa-berniceb.repl.co/",icon:"pisay.png"}
 
-links[25]={section:"9Sr",title:"Air Disaster",url:"https://srmariano2ndqtrproj.sophiamariano1.repl.co/index.html",icon:"air2.png"}
-links[26]={section:"9Sr",title:"Lakbay Alamat",url:"https://cs3-borja-q1.gweniscoding26.repl.co/index.html",icon:"alamat.png"}
-links[27]={section:"9Sr",title:"Terra",url:"https://cs3-cipriano-q1.hans-gabrielga2.repl.co/index.html",icon:"terra.png"}
-links[28]={section:"9Sr",title:"The Safe Space",url:"https://cs3-cipriano-q1.hans-gabrielga2.repl.co/index.htmll",icon:"safespace.png"}
-links[29]={section:"9Sr",title:"Hiraya: OPM Underground Scene",url:"https://bemanalaysayhiraya--alicia-louiselo.repl.co/",icon:"hiraya.png"}
-links[30]={section:"9Sr",title:"Art Scheme by Ashton",url:"https://my-art-scheme--ashton-oliverol.repl.co/",icon:"myartscheme.png"}
-links[31]={section:"9Sr",title:"Loonation",url:"https://loona--vincent-joaquin.repl.co/",icon:"loonation.png"}
-links[32]={section:"9Sr",title:"Rubiks Cube For Noobs",url:"https://rubiks-cube-for-noobs.jatzafra.repl.co/",icon:"rubiks.png"}
-links[33]={section:"9Sr",title:"David's Math Tutorials",url:"https://cs3-project--nathanael-noahd.repl.co/index.html",icon:"math.png"}
-links[34]={section:"9Sr",title:"Seeking Answers from Singapore's Success",url:"https://project-1.kristner-sheyns.repl.co/index.html",icon:"singapore.png"}
-links[35]={section:"9Sr",title:"Cubing 3p",url:"https://cubing-3p.ghillianne-kate.repl.co/",icon:"cubing.png"}
-links[36]={section:"9Sr",title:"All You Need To Know About Camping",url:"https://all-you-need-to-know-about-camping--iris-lexilexi2.repl.co/",icon:"camping.png"}
-links[37]={section:"9Sr",title:"Pentips: Where Your Writing Begins",url:"https://pentips-where-your-writing-journey-begins--zoe-alexaalexa.repl.co/",icon:"pentips.png"}
-links[38]={section:"9Sr",title:"Pokemon Basics",url:"https://the-basics-of-pokemon-black-and-white.pjpdorango.repl.co/",icon:"pokemon.png"}
-links[38]={section:"9Sr",title:"Realism in Video Games",url:"https://a-1.paul-zijiroziji.repl.co/",icon:"realism.png"}
 
 function dispProj(){
 	var x;
