269e829be1519a0f27ae364f5396161ad9d139f0
atlmendoza <94661595+atlmendoza@users.noreply.github.com>
2025-02-06 11:11:42 +0800

    Update links.js

--- links.js
+++ links.js
@@ -5,6 +5,7 @@ links[1]={section:"9Cs",title:"CipherMania",desc:"Learn more about our inspirati
 links[2]={section:"9Cs",title:"Mindless Motivation",desc:"Mindless Motivation is a mental health website where struggling students can open as a source for emotions.",url:"https://typical-pentagonal-vessel.glitch.me/",icon:"mindless.png",by:"angeles&esguerra"}
 links[3]={section:"9Cs",title:"Aklatan-Mo: Aklatan-Ko",desc:"Your gateway to a world of literature. Start reading our collection of top-rated books across various genres, from classics to bestsellers.",url:"https://geroge-real.github.io/AMAKcs/public/",icon:"aklatan.png",by:"froyalde&adriano"}
 links[4]={section:"9Cs",title:"Cherry On Top",desc:"Cherry on Top is an interactive game where users can decorate and display their own sweet treat.",url:"https://cs3-proj-cesium-luz-triguero.glitch.me/homepage.html",icon:"cherryontop.jpg",by:"luz&triguero"}
+links[5]={section:"9Cs",title:"FitFormula",desc:"FitFormula,  is designed to help users start and go through their excercise and fitness experience by providing calculation tools,  plans according to the user's fitness situation, and other pages.",url:"https://elijahandrei.github.io/Q3DimayugaMasangkayProject/public/index.html",icon:"cherryontop.jpg",by:"dimayuga&msangkay"}
 
 
 function dispProj(){
